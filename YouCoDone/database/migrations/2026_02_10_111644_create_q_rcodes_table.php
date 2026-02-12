214aa4163ee2e4f27cda586e4bdde002b7540425
ElMehdiCherkaoui <cherkaouim451@gmail.com>
2026-02-12 09:15:21 +0100

    Add migration files for users, cache, jobs, permissions, restaurants, menus, menu items, favorites, menu-item relationships, reservations, notifications, payments, and QR codes

---
+++ YouCoDone/database/migrations/2026_02_10_111644_create_q_rcodes_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('q_rcodes', function (Blueprint $table) {
+            $table->id();
+            $table->string('file_path');
+            $table->foreignId('reservation_id')->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('q_rcodes');
+    }
+};
