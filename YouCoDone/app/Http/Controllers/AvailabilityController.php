209b7fa755843ba9702b0a8943f1d461709a55c3
ElMehdiCherkaoui <cherkaouim451@gmail.com>
2026-02-13 16:24:26 +0100

    Refactor AvailabilityController to include restaurant dependency in index and store methods

--- YouCoDone/app/Http/Controllers/AvailabilityController.php
+++ YouCoDone/app/Http/Controllers/AvailabilityController.php
@@ -4,16 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+USE App\Models\Availability;
+use App\Models\Restaurant;
 
 class AvailabilityController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
-    {
-        return view('restaurateur.availabilities');
-    }
+public function index(Restaurant $restaurant)
+{
+    return view('restaurateur.availabilities', compact('restaurant'));
+}
+
 
     /**
      * Show the form for creating a new resource.
@@ -26,9 +29,21 @@ class AvailabilityController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request, Restaurant $restaurant)
     {
-        //
+
+        $validated = $request->validate([
+            'date' => 'required|date',
+            'start_time' => 'required',
+            'end_time' => 'required|after:start_time',
+            'is_available' => 'required|boolean',
+        ]);
+        $validated['restaurants_id'] = $restaurant->id;
+        $validated['restaurants_id'] = $restaurant->id;
+
+        Availability::create($validated);
+
+        return back()->with('success', 'Schedule added successfully.');
     }
 
     /**
