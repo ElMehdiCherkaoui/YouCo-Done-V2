0ef6c77887fe0aa0d8b06c83b4e26f5ef9a544ae
ElMehdiCherkaoui <cherkaouim451@gmail.com>
2026-02-13 16:30:13 +0100

    Enhance PaymentController with payment success handling, notification creation, and invoice generation

--- YouCoDone/app/Http/Controllers/PaymentController.php
+++ YouCoDone/app/Http/Controllers/PaymentController.php
@@ -4,15 +4,83 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Reservation;
+use App\Models\Restaurant;
+use Illuminate\Support\Facades\Log;
+use App\Models\Notification;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\ReservationPaidMail;
 
 class PaymentController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Restaurant $restaurant, Reservation $reservation)
     {
-        return view("client.payment");
+        return view('client.payment', compact('restaurant', 'reservation'));
+    }
+
+
+
+    public function success(Request $request)
+    {
+        $reservation = Reservation::findOrFail($request->reservation);
+
+        $reservation->update([
+            'payment_status' => true,
+        ]);
+
+        $restaurant = $reservation->restaurant;
+
+        Notification::create([
+            'type' => 'payment',
+            'message' => 'New reservation payment confirmed.',
+            'reservation_id' => $reservation->id,
+            'user_id' => $restaurant->user_id,
+        ]);
+
+        Mail::to($reservation->user->email)
+            ->send(new ReservationPaidMail($reservation, $restaurant));
+
+        return view('client.confirmation', compact('reservation', 'restaurant'));
+    }
+
+
+    public function downloadInvoice(Reservation $reservation)
+    {
+        $pdf = new \FPDF();
+        $pdf->AddPage();
+        $pdf->SetFont('Arial', 'B', 16);
+
+        $pdf->Cell(40, 10, 'Reservation Invoice');
+        $pdf->Ln();
+
+        $pdf->SetFont('Arial', '', 16);
+        $pdf->Cell(10, 10, 'Mr: ' . $reservation->user->name);
+        $pdf->Ln();
+        $pdf->Cell(10, 10, 'Restaurant: ' . $reservation->restaurant->name);
+        $pdf->Ln();
+        $pdf->Cell(10, 10, 'city: ' . $reservation->restaurant->city);
+        $pdf->Ln();
+        $pdf->Cell(10, 10, 'location: ' . $reservation->restaurant->address);
+        $pdf->Ln();
+        $pdf->Cell(10, 10, 'Date: ' . $reservation->reservation_date);
+        $pdf->Ln();
+        $pdf->Cell(10, 10, 'Time: ' . $reservation->time_solt);
+        $pdf->Ln();
+        $pdf->Cell(10, 10, 'People: ' . $reservation->number_of_people);
+
+        return response($pdf->Output())
+            ->header('Content-Type', 'application/pdf');
+    }
+
+
+
+
+    public function cancel()
+    {
+        return redirect()->back()->with('error', 'Payment was cancelled.');
     }
 
     /**
